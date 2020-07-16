7298507fcff133cdc825ecdca8a206485f095087
Hong Nguyen <nguyenthanhhong26@gmail.com>
2020-07-16 17:04:50 +0700

    Initial commit

--- SpringSecurityOauth2Login/src/main/java/com/nth/SpringSecurityOauth2Login/SpringSecurityOauth2LoginApplication.java
+++ SpringSecurityOauth2Login/src/main/java/com/nth/SpringSecurityOauth2Login/SpringSecurityOauth2LoginApplication.java
@@ -2,9 +2,16 @@ package com.nth.SpringSecurityOauth2Login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSecurityOauth2LoginApplication {
+public class SpringSecurityOauth2LoginApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringSecurityOauth2LoginApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityOauth2LoginApplication.class, args);
