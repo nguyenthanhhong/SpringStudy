0bc6f72ea292e32db69a0efc198d3b83d05c5763
Hong Nguyen <nguyenthanhhong26@gmail.com>
2020-07-21 10:01:22 +0700

    Initial commit

---
+++ SpringBatch/src/main/java/com/nth/SpringBatch/SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nth.SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
---
+++ SpringBatchDynamicSchedule/src/test/java/com/nth/SpringBatchDynamicSchedule/DynamicScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nth.SpringBatchDynamicSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
